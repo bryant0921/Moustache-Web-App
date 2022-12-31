9a96b1986e1618e3b7f70848313f3cc1d4765701
bryant0921 <97357589+bryant0921@users.noreply.github.com>
2022-12-31 17:41:45 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX = 0;
+noseY = 0;
+function preload()
+{
+    moustache = loadImage('https://i.postimg.cc/6QYzCxgJ/m.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image (video, 0, 0, 300, 300);
+    image(moustache,noseX,noseY,80,35)
+}
+
+function takeSnapshot()
+{
+    save('myfilter.png');
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 40 ;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X =" + noseX);
+        console.log("nose Y =" + noseY);
+    }
+}
